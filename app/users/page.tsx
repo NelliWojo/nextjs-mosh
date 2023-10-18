910e4c349287c29b3fdda0f89953f7087b6e73e8
NelliWojo <nelli.timorshina@wojo.com>
2023-10-18 17:55:08 +0200

    fetch data, static & dynamic rendering

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,7 +1,30 @@
 import React from "react";
 
-const UsersPage = () => {
-  return <div>UsersPage</div>;
+interface User {
+  id: number;
+  name: string;
+}
+
+const UsersPage = async () => {
+  const response = await fetch(
+    "https://jsonplaceholder.typicode.com/users",
+    // {cache: 'no-cache'}, // possible only w/fetch
+    // data is cached by default
+    { next: { revalidate: 10 } } // revalidate data every 10 seconds
+  );
+  const users: User[] = await response.json();
+
+  return (
+    <div>
+      <h1>Users</h1>
+      <p>{new Date().toLocaleTimeString()}</p>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default UsersPage;
