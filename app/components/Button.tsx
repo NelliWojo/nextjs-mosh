66e95683e16e034c62417c55c31f891ec6844ea6
NelliWojo <nelli.timorshina@wojo.com>
2023-10-18 17:17:48 +0200

    init, new page & components

---
+++ app/components/Button.tsx
@@ -0,0 +1,8 @@
+"use client";
+import React from "react";
+
+const Button = () => {
+  return <button onClick={() => console.log("Click")}>Add To Cart</button>;
+};
+
+export default Button;
---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Button from "./Button";
+
+const ProductCard = () => {
+  return (
+    <>
+      <h1>ProductCard</h1>
+      <Button />
+    </>
+  );
+};
+
+export default ProductCard;
